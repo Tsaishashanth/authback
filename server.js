00165e3788e02f98e5b4b143d24ecfd40969be71
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-07-04 12:41:27 +0530

    added sever is running

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 //starting the server
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {console.log(`🚀 Server running on port ${PORT}`)});
