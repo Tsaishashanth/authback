1c994003603be3d07ead69e52fcdb387eb0a8979
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-07-14 20:43:55 +0530

    updated apis

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ router.post('/signup', async (req, res) => {
 
     if(error) return res.status(400).json({error: error.message, success:false});
 
-    res.json({message: 'User signed up', user:data.user, accessToken: data.session?.access_token,success:true});
+    res.json({message: 'User signed up', user:{id:user.id,email:email}, accessToken: data.session?.access_token,success:true});
 });
 
 
@@ -22,15 +22,23 @@ router.post('/login', async (req, res) => {
 
   if (error) return res.status(400).json({ error: error.message,success: false });
 
-  res.json({ message: 'Login successful',accessToken: data.session?.access_token, user: data.user,success:true});
+  res.json({ message: 'Login successful',user:{id:user.id, email:email},accessToken: data.session?.access_token, success:true});
 });
 
 
 //user details
- router.get('/user', async (req,res) => {
+ router.post('/user', async (req,res) => {
   const token = req.headers.authorization?.split(' ')[1]; 
 
-  if(!token) return res.status(401).json({error: 'token unavailable',message: unauthorized});
+  if(!token) return res.status(401).json({error: 'token unavailable',message: 'unauthorized'});
+
+  const{email, password} = req.body;
+
+  const{data, error:loginError} = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+  if(loginError) return res.status(400).json({error: "invalid credentials"});
 
   try{
     const{ data: {user}, error} = await supabase.auth.getUser(token);
@@ -46,6 +54,7 @@ router.post('/login', async (req, res) => {
 
 module.exports = router;
 
+
 //updateuser
 
 
@@ -54,14 +63,26 @@ router.put('/updateuser', async (req,res) => {
   // email should be the same and password should be different
 
 
-  const {email,newpassword } = req.body;  
+  const {email,newpassword, oldpassword } = req.body;  
+
+  // pass old password if not send error
 
   if(!token) return res.status(401).json({error: 'no token provided'});
 
   const {data : {user}, error: userError} = await supabase.auth.getUser(token);
   if(userError) return res.status(401).json({error: userError.message});
 
-  await supabase.auth.setSession({ access_token: token });
+  const{data:sessionData, error:signInError} = await supabase.auth.signInWithPassword({
+    email:user.email,
+    password: oldpassword
+  });
+
+  if (signInError) {
+    return res.status(401).json({error: "old password is incorrect"
+    });
+  }
+
+  // await supabase.auth.setSession({ access_token: token }); no need this because we are already using admin 
 
   const updatedata = {};
   if(email) updatedata.email = email;
@@ -84,10 +105,21 @@ router.post('/logout', async(req,res) =>{
 
   if(!token) return res.status(401).json({error: 'no token provided'});
 
+  const{email, password} = req.body;
+
+  const{data, error:loginError} = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if(loginError) res.status(401).json({error: "invalid credentials"})
+
   const{error} = await supabase.auth.signOut();
+
   if(error)
     return res.status(400).json({error:error.message});
 
   res.json({message: 'User logged out Successfully'});
 });
+
 
